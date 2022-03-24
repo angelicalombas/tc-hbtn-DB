692e76742dc7df33344200cb039bac3082fb2dac
angelicalombas <angelica_lombas@hotmail.com>
2022-03-24 09:46:31 -0300

    exercicio 0

--- 0x01/java_jdbc/PrintJDBCDrivers.java
+++ 0x01/java_jdbc/PrintJDBCDrivers.java
@@ -12,4 +12,15 @@ public class PrintJDBCDrivers {
         }
     }
 
+    public static void print(Driver driver) {
+        String className = driver.getClass().getName();
+        int majorVersion = driver.getMajorVersion();
+        int minorVersion = driver.getMinorVersion();
+        System.out.println("----------------------------------------");
+        System.out.println("Name Driver: " + className);
+        System.out.println("Driver Major Version: " + majorVersion);
+        System.out.println("Driver Minor Version: " + minorVersion);
+        System.out.println("----------------------------------------");
+    }
+
 }
