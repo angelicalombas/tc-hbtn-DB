15462b3e1201e8383116b3712b5f4b6de740c577
angelicalombas <angelica_lombas@hotmail.com>
2022-03-24 09:44:21 -0300

    exercicio 0

--- 0x01/java_jdbc/PrintDrivers.java
+++ 0x01/java_jdbc/PrintDrivers.java
@@ -1,15 +1,17 @@
 import java.sql.Driver;
-import java.sql.DriverManager;
-import java.util.Enumeration;
+
 
 
 public class PrintDrivers {
 
-    public static void main(String[] args) {
-        System.out.println("List of loaded JDBC drivers");
-        for (Enumeration<Driver> e = DriverManager.getDrivers(); e.hasMoreElements();) {
-            Driver driver = e.nextElement();
-            System.out.println(driver);
-        }
+    public static void print(Driver driver) {
+        String className = driver.getClass().getName();
+        int majorVersion = driver.getMajorVersion();
+        int minorVersion = driver.getMinorVersion();
+        System.out.println("----------------------------------------");
+        System.out.println("Name Driver: " + className);
+        System.out.println("Driver Major Version: " + majorVersion);
+        System.out.println("Driver Minor Version: " + minorVersion);
+        System.out.println("----------------------------------------");
     }
 }
