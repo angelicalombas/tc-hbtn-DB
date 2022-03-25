db217f8d45d3d50ead476172e86493684880a542
angelicalombas <angelica_lombas@hotmail.com>
2022-03-25 08:25:48 -0300

    exercicio 0

---
+++ 0x02/java_jdbc_manipulation/src/Cliente.java
@@ -0,0 +1,38 @@
+public class Cliente {
+    private String nome;
+    private Integer idade;
+    private String CPF;
+    private String RG;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getRG() {
+        return RG;
+    }
+
+    public void setRG(String RG) {
+        this.RG = RG;
+    }
+}
